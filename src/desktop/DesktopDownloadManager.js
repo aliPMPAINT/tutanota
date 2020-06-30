8697b1b4e2d4c84b0f8b4fdd01748363f8d8db29
nig <nig@tutao.de>
2020-06-30 14:49:05 +0200

    properly close file after native download attempt, fix #2113
    resolving the downloadNative promise after the stream was explicitly
    closed should prevent any EBUSY errors from cropping up when the user
    chose 'open attachment' and the client tries to open the file right
    after the download.

--- src/desktop/DesktopDownloadManager.js
+++ src/desktop/DesktopDownloadManager.js
@@ -30,29 +30,41 @@ export class DesktopDownloadManager {
 		return new Promise((resolve, reject) => {
 			fs.mkdirp(app.getPath('temp') + '/tuta/')
 			const encryptedFileUri = path.join(app.getPath('temp'), '/tuta/', fileName)
-			this._net.request(sourceUrl, {
-				method: "GET",
-				headers,
-				timeout: 20000
-			}).on('response', res => {
-				let fileStream = fs.createWriteStream(encryptedFileUri)
-				res.pipe(fileStream)
-				fileStream.on('finish', () => {
-					fileStream.close(() => resolve({
-						statusCode: res.statusCode,
-						statusMessage: res.statusMessage,
-						encryptedFileUri
-					}))
-				})
-			}).on('error', e => {
-				// remove file if it was already created
-				fs.unlink(encryptedFileUri).catch(noOp)
-				reject(e)
-			}).end()
+			const fileStream = fs
+				.createWriteStream(encryptedFileUri, {emitClose: true})
+				.on('finish', () => fileStream.close()) // .end() was called, contents is flushed -> release file desc
+			let cleanup = e => {
+				cleanup = noOp
+				fileStream.removeAllListeners('close').on('close', () => { // file descriptor was released
+					fileStream.removeAllListeners('close')
+					// remove file if it was already created
+					fs.unlink(encryptedFileUri).catch(noOp).then(() => reject(e))
+				}).end() // {end: true} doesn't work when response errors
+			}
+			this._net.request(sourceUrl, {method: "GET", timeout: 20000, headers})
+			    .on('response', response => {
+				    response.on('error', cleanup)
+				    if (response.statusCode !== 200) {
+					    // causes 'error' event
+					    response.destroy(response.statusCode)
+					    return
+				    }
+				    response.pipe(fileStream, {end: true}) // automatically .end() fileStream when dl is done
+				    const result = {
+					    statusCode: response.statusCode,
+					    statusMessage: response.statusMessage,
+					    encryptedFileUri
+				    }
+				    fileStream.on('close', () => resolve(result))
+			    }).on('error', cleanup).end()
 		})
 	}
 
-	open(itemPath: string): Promise<void> {
+	open(itemPath
+		     :
+		     string
+	):
+		Promise<void> {
 		const tryOpen = () => {
 			if (shell.openItem(itemPath)) {
 				return Promise.resolve()
@@ -60,7 +72,8 @@ export class DesktopDownloadManager {
 				return Promise.reject(new FileOpenError("Could not open " + itemPath))
 			}
 		}
-		if (DesktopUtils.looksExecutable(itemPath)) {
+		if (DesktopUtils.looksExecutable(itemPath)
+		) {
 			return dialog.showMessageBox(null, {
 				type: "warning",
 				buttons: [lang.get("yes_label"), lang.get("no_label")],
@@ -78,7 +91,15 @@ export class DesktopDownloadManager {
 		}
 	}
 
-	saveBlob(filename: string, data: Uint8Array, win: ApplicationWindow): Promise<void> {
+	saveBlob(filename
+		         :
+		         string, data
+		         :
+		         Uint8Array, win
+		         :
+		         ApplicationWindow
+	):
+		Promise<void> {
 		const write = ({canceled, filePath}): Promise<void> => {
 			if (!canceled) return fs.writeFile(filePath, data)
 			return Promise.reject('canceled')
@@ -97,11 +118,18 @@ export class DesktopDownloadManager {
 	}
 
 
-	_handleDownloadItem(ev: Event, item: DownloadItem): void {
+	_handleDownloadItem(ev
+		                    :
+		                    Event, item
+		                    :
+		                    DownloadItem
+	):
+		void {
 		const defaultDownloadPath = this._conf.getDesktopConfig('defaultDownloadPath')
 		// if the last dl ended more than 30s ago, open dl dir in file manager
 		let fileManagerLock = noOp
-		if (defaultDownloadPath && fs.existsSync(defaultDownloadPath)) {
+		if (defaultDownloadPath && fs.existsSync(defaultDownloadPath)
+		) {
 			try {
 				const fileName = path.basename(item.getFilename())
 				const savePath = path.join(
